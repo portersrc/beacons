b66d91e15e6c7fcf8d95387a69304653fd2c7a12
chris porter <cporter35@gatech.edu>
2018-06-16 17:25:52 -0400

    use 60s runs

--- ubench/repeat_run.sh
+++ ubench/repeat_run.sh
@@ -2,5 +2,6 @@
 
 for mem_usage_kb in `seq 50 50 1000`; do
     #{ time ./run.sh $mem_usage_kb 1 10 ; } &> result_$mem_usage_kb.txt
-    { time ./run.sh $mem_usage_kb 1 1000 ; } &> result_$mem_usage_kb.txt
+    #{ time ./run.sh $mem_usage_kb 1 1000 ; } &> result_$mem_usage_kb.txt
+    ./run.sh $mem_usage_kb 0 60 &> result_$mem_usage_kb.txt
 done
