677f9231245c6a5e67f787cf85eb040b93d64eb6
chris porter <cporter35@gatech.edu>
2018-06-16 16:54:00 -0400

    add wrapper for run.sh to get timing results across a range of memory footprints

---
+++ ubench/repeat_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for mem_usage_kb in `seq 50 50 1000`; do
+    #{ time ./run.sh $mem_usage_kb 1 10 ; } &> result_$mem_usage_kb.txt
+    { time ./run.sh $mem_usage_kb 1 1000 ; } &> result_$mem_usage_kb.txt
+done
