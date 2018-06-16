9088ffb36d8424dcc3cf5065bcb7d5a2dfdd3ab0
chris porter <cporter35@gatech.edu>
2018-06-16 16:11:56 -0400

    add arg to run script

--- ubench/run.sh
+++ ubench/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 mem_usage_kb=$1
-timeout=$2
+is_loopcount_based=$2
+timeout=$3
 
 for core in {0..47}; do
-    taskset -c $core ./mat_mult $mem_usage_kb $timeout &
+    taskset -c $core ./mat_mult $mem_usage_kb $is_loopcount_based $timeout &
 done
 
 wait
