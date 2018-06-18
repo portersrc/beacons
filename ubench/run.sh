924a76f5db04fb0914e677bc2081bf0da5cba823
chris porter <cporter35@gatech.edu>
2018-06-17 22:45:23 -0400

    adjust run.sh to taskset on every other core; checking if this is how the two thunderx's got mapped

--- ubench/run.sh
+++ ubench/run.sh
@@ -4,7 +4,8 @@ mem_usage_kb=$1
 is_loopcount_based=$2
 timeout=$3
 
-for core in {0..47}; do
+#for core in {0..47}; do
+for core in `seq 0 2 95`; do
     taskset -c $core ./mat_mult $mem_usage_kb $is_loopcount_based $timeout &
 done
 
